8a4ff33b8c3442b51b70c4e521c58b0f98563833
Besim Strazimiri <besimstrazimiri@outlook.fr>
2026-01-06 08:54:04 +0100

    commit #2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World");
+﻿/*Thread mainthread = Thread.CurrentThread;
+
+Console.WriteLine($"ID: {mainthread.ManagedThreadId}, State: {mainthread.ThreadState}, Name: {mainthread.Name}");
+
+mainthread.Name = "TPthread";
+Console.WriteLine($"Nouveau nom: {mainthread.Name}");
+*/
+
+
+static void affiche()
+{
+    while (true)
+    {
+        Console.WriteLine($"Thread name: {Thread.CurrentThread.Name}");
+        Thread.Sleep(1000);
+    }
+}
+Thread Th1 = new Thread(affiche);
+Th1.Name = "Thread 1";
+Th1.Start();
+
+Thread Th2 = new Thread(affiche);
+Th2.Name = "Thread 2";
+Th2.Start();
+
+
+
