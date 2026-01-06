afdae5f94ff525bc427f2a862677b77190d6e38c
Besim Strazimiri <besimstrazimiri@outlook.fr>
2026-01-06 10:47:28 +0100

    commit #3

---
+++ CTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPthreadCSharp
+{
+    public class CTest
+    {
+
+        public void afficheB()
+            {
+                while (true)
+                {
+                    Console.WriteLine($"Thread name: {Thread.CurrentThread.Name}");
+                    Thread.Sleep(1000);
+                }
+            }
+        public void afficheC()
+        {
+            while (true)
+            {
+                Thread thread = Thread.CurrentThread;
+                for (int i = 0; i < 101; i++)
+                {
+                    Console.WriteLine($"Thread name: {Thread.CurrentThread.Name}");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ Console.WriteLine($"Nouveau nom: {mainthread.Name}");
 */
 
 
+using TPthreadCSharp;
+
 static void affiche()
 {
     while (true)
@@ -22,6 +24,18 @@ Th1.Start();
 Thread Th2 = new Thread(affiche);
 Th2.Name = "Thread 2";
 Th2.Start();
+
+CTest test = new CTest();
+
+Thread Th3 = new Thread(test.afficheB);
+Th3.Name = "thread 3";
+Th3.Start();
+
+Thread Th4 = new Thread(test.afficheC);
+Th4.Name = "thread 4";
+Th4.Start();
+
+
 
 
 
